1f4a89ce5c94fbf9a77ae5c6ab73673340ce8a5e
Atanu <atanum991@gmail.com>
2024-04-02 13:33:39 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// hero section ----------------------------------------------------
 gsap.to(
     "#name", {
         opacity: 1,
@@ -13,6 +14,14 @@ gsap.to(
         y: 10,
     }
 )
+gsap.to(
+        "#vk", {
+            opacity: 1,
+            delay: 1,
+            duration: 1,
+        }
+    )
+    // rise ----------------------------------------------------
 gsap.to(
     "#img1", {
         scale: 1,
@@ -40,17 +49,18 @@ gsap.to(
     }
 )
 gsap.to(
-    "#p1", {
-        x: 0,
-        scrollTrigger: {
-            trigger: "#p1",
-            start: "top 85%", // animation starts when the top touches the 60% of the viewport height
-            end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
-            toggleActions: "play reverse restart reverse", // play pause resume reverse restart reset complete
-            // onEnter onLeave onEnterBack onLeaveBack
+        "#p1", {
+            x: 0,
+            scrollTrigger: {
+                trigger: "#p1",
+                start: "top 85%", // animation starts when the top touches the 60% of the viewport height
+                end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
+                toggleActions: "play reverse restart reverse", // play pause resume reverse restart reset complete
+                // onEnter onLeave onEnterBack onLeaveBack
+            }
         }
-    }
-)
+    )
+    // rise ----------------------------------------------------
 gsap.to(
     "#img2", {
         scale: 1,
@@ -78,18 +88,18 @@ gsap.to(
     }
 )
 gsap.to(
-    "#p2", {
-        x: 0,
-        scrollTrigger: {
-            trigger: "#p2",
-            start: "top 85%", // animation starts when the top touches the 60% of the viewport height
-            end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
-            toggleActions: "play reverse restart reverse", // play pause resume reverse restart reset complete
-            // onEnter onLeave onEnterBack onLeaveBack
+        "#p2", {
+            x: 0,
+            scrollTrigger: {
+                trigger: "#p2",
+                start: "top 85%", // animation starts when the top touches the 60% of the viewport height
+                end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
+                toggleActions: "play reverse restart reverse", // play pause resume reverse restart reset complete
+                // onEnter onLeave onEnterBack onLeaveBack
+            }
         }
-    }
-)
-
+    )
+    // rise ----------------------------------------------------
 gsap.to(
     "#img3", {
         scale: 1,
@@ -116,17 +126,18 @@ gsap.to(
     }
 )
 gsap.to(
-    "#p3", {
-        x: 0,
-        scrollTrigger: {
-            trigger: "#p3",
-            start: "top 85%", // animation starts when the top touches the 60% of the viewport height
-            end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
-            toggleActions: "restart none none none", // play pause resume reverse restart reset complete
-            // onEnter onLeave onEnterBack onLeaveBack
+        "#p3", {
+            x: 0,
+            scrollTrigger: {
+                trigger: "#p3",
+                start: "top 85%", // animation starts when the top touches the 60% of the viewport height
+                end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
+                toggleActions: "restart none none none", // play pause resume reverse restart reset complete
+                // onEnter onLeave onEnterBack onLeaveBack
+            }
         }
-    }
-)
+    )
+    // scrubbing ----------------------------------------------------
 gsap.to(
     "#ag", {
         x: -1200,
@@ -134,7 +145,7 @@ gsap.to(
             trigger: "#scrubbing",
             start: "top 70%", // animation starts when the top touches the 60% of the viewport height
             end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
-            scrub: 4,
+            scrub: 2,
             toggleActions: "restart none none none", // play pause resume reverse restart reset complete
             // onEnter onLeave onEnterBack onLeaveBack
         }
@@ -147,7 +158,7 @@ gsap.to(
             trigger: "#scrubbing",
             start: "top 70%", // animation starts when the top touches the 60% of the viewport height
             end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
-            scrub: 4,
+            scrub: 2,
             toggleActions: "restart none none none", // play pause resume reverse restart reset complete
             // onEnter onLeave onEnterBack onLeaveBack
         }
@@ -160,16 +171,16 @@ gsap.to(
             trigger: "#scrubbing",
             start: "top 70%", // animation starts when the top touches the 60% of the viewport height
             end: "bottom 10%", // animation ends when the bottom touches the 30% of the viewport height
-            scrub: 4, //can use 'true' 
+            scrub: 2, //can use 'true' 
             toggleActions: "play reverse restart reverse", // play pause resume reverse restart reset complete
             // onEnter onLeave onEnterBack onLeaveBack
         }
     }
 )
 
-
+//typedJS ----------------------------------------------------
 var typed = new Typed('#quote', {
     strings: ['Self-belief and hard work will always earn you success.', 'I never fear failure, I only fear not trying.', 'Your fear of failure can be your greatest motivator.', 'You have to be fearless and take risks in order to achieve greatness.', 'Passion and hard work can overcome any obstacle.', 'There is always room for improvement, no matter how good you think you are.', 'Success is not just about talent, it is about persistence and determination.', 'Do not let success get to your head, stay humble and grounded.', 'I do not believe in luck, I believe in hard work and preparation.'],
-    typeSpeed: 80,
+    typeSpeed: 50,
     loop: true,
 });
